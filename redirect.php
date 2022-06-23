ff06cbb4e8da7386d52adb310aaebde8fd0a03cd
michael alemayehu <mikealexiv565@gmail.com>
2022-06-23 17:52:23 +0300

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,13 +1,13 @@
 <?php function Redirect($url, $statusCode=303){
-    $redirect_url = parse_url($url);
     $urlPattern = "#^http://localhost:3000/#";
 
     if (!preg_match($urlPattern,$url)){
-     showNotification($url);
-    die();
-
+     showNotification("erroneous url!!");
+     die();
     }else{
     header('Location: ' .$url, true, $statusCode);
+    die();
     }
+    die();
 } 
 ?>
